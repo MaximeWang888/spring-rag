3ac00659357090c55820a5ffc269bbf1b6196ac6
Maxime Wang <maxime.wang@efrei.net>
2025-01-29 13:26:51 +0100

    Add K8S deployment.yml and add step in build.yml for GitHub Action Pipeline

--- src/main/java/com/efrei/rag/RagApplication.java
+++ src/main/java/com/efrei/rag/RagApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.efrei.rag;
+package main.com.efrei.rag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
